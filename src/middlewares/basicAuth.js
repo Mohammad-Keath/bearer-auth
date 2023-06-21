35bb6cb7b8597fb0ffc4daf5b7753566c2743898
Mohammad-Keath <m.ghyath.z@gmail.com>
2023-06-21 13:38:34 +0000

    bearer auth

---
+++ src/middlewares/basicAuth.js
@@ -0,0 +1,20 @@
+const base64 = require('base-64');
+const userModel = require('../models/users.model');
+
+function basicAuth (req,res,next){
+    if(req.headers.authorization){
+      let encoded = req.headers.authorization.split(' ')
+      let code = encoded.pop()
+      let decoded = base64.decode(code)
+      let [username,password] = decoded.split(':')
+      console.log(username)
+      userModel.authBasic(username,password).then(data=>{
+        req.user = data
+        next()
+      }).catch((error)=>{
+        next('invalid Login:'+error.message)
+      })
+      
+    }else next("invalid Login: you are not signing in")
+}
+module.exports= basicAuth
